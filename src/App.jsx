cd3c9d69fa6232f46165733690abd5fd15b97053
Tyler <tylerfarhner@gmail.com>
2023-01-28 20:54:56 -0800

    shoulda committed more sooner

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <a>
+        <p>
+          Evergreen Can Clean
+        </p>
+      </a>
+    </div>
+  )
+}
+
+export default App
